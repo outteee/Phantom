ece90457c80690c6b952dce2854fee31aafef2a4
outteee <outteesteam@gmail.com>
2026-07-11 23:32:05 -0400

    loader.lua fixed.

--- loader.lua
+++ loader.lua
@@ -13,7 +13,7 @@ elseif ID == 2548183080 then
 elseif ID == 5456952508 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/outteee/Phantom/main/valley.lua"))()
     gamee = "Valley Prison"
-elseif ID == nil then
+elseif ID == 2339944792 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/outteee/Phantom/main/lurkgiants.lua"))()
     gamee = "Lurking Giants"
 end
